f809f884dd177b9cd4d79f0cd5d25582541af110
Kevin Kurpe <kevin@thinkgrasp.com>
2016-09-14 15:55:00 -0400

    Added the BCPaymentMethod and BCDirectory models. These models will be used to create and edit customer and payment profiles.

--- model.rb
+++ model.rb
@@ -7,3 +7,13 @@ class BCPaymentDate < Rfm::Base
 	config :database => 'Data'
 	config :layout => 'T54_WEB_PAYMENTDATE'
 end
+
+class BCPaymentMethod < Rfm::Base
+	config :database => 'Data'
+	config :layout => 'T55_WEB_PAYMENTMETHOD'
+end
+
+class BCDirectory < Rfm::Base
+	config :database => 'Data'
+	config :layout => 'T55_WEB_DIRECTORY'
+end
