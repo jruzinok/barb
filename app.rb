698f328a1953c0bde58e952f6296eb65b03ce215
Kevin Kurpe <kevin@thinkgrasp.com>
2016-09-14 19:05:52 -0400

    Created a new Profile Sinatra app to handle the creation of customer and payment profile requests.

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ require 'openssl'
 # This loads the Ruby 2 FileMaker server configuration settings.
 config = YAML.load_file(File.dirname(__FILE__) + "/config/rfm.yml")
 
-require_relative 'paymentdate_model.rb'
+require_relative 'model.rb'
 require_relative 'paymentdate_controller.rb'
 require_relative 'paymentdate_process.rb'
 
@@ -20,16 +20,13 @@ class CreditCard < Sinatra::Application
 		@batch = params[:batch]
 		process
 	end
+end
 
-	# FUTURE DEVELOPMENT
-	# get '/:auth/:batch' do
-		# @auth = params[:auth]
-		# @batch = params[:batch]
-		# if @auth == env['APP_PASSWORD']
-		# 	process (@batch)
-		# else
-		# 	redirect 'www.google.com'
-		# end
-	# end
+class Profiles < Sinatra::Application
 
+	get '/create-customer-profile/:database/:directory_id' do
+		@database = params[:database]
+		@directory_id = params[:directory_id]
+		create_customer_profile
+	end
 end
