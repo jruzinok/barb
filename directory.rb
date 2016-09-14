0f689c4c0b300ad251a967092634cbbccbc52009
Kevin Kurpe <kevin@thinkgrasp.com>
2016-09-14 19:07:22 -0400

    Updated the find_directory method to trap for record not found. Added the missing end in the create_customer_profile method.

--- directory.rb
+++ directory.rb
@@ -1,37 +1,41 @@
 def create_customer_profile
 	find_directory
 
-	if @has_profile == false
+	if @record_found = true && @has_profile == false
 		request = CreateCustomerProfileRequest.new
 		request.profile = CustomerProfileType.new(@directory_id,@namefull,nil, nil)
 
 		@response = transaction.create_customer_profile(request)
 
-	# The transaction has a response.
-	if @response.messages.resultCode == MessageTypeEnum::Ok
-		@responseKind = "OK"
-		@profile_id = @response.customerProfileId
-	else
-		@responseKind = "ERROR"
-		@responseCode = @response.messages.messages[0].code
-		@responseError = @response.messages.messages[0].text
-	end
+		# The transaction has a response.
+		if @response.messages.resultCode == MessageTypeEnum::Ok
+			@responseKind = "OK"
+			@profile_id = @response.customerProfileId
+		else
+			@responseKind = "ERROR"
+			@responseCode = @response.messages.messages[0].code
+			@responseError = @response.messages.messages[0].text
+		end
 
-	update_directory
-	clear_response
+		update_directory
+		clear_response
+	end
 end
 
 def find_directory
 	if @database == "BC"
-		@directory = BCDirectory.find(:__kP_Directory => @id)
+		@directory = BCDirectory.find(:__kP_Directory => @directory_id)
 
-		# Load the individual values.
-		load_directory
+		if @directory[0] != nil
+			@record_found = true
+			load_directory
+		else
+			@record_found = false
+		end
 	end
 end
 
 def load_directory
-	@directory_id = @directory["__kP_Directory"]
 	@namefirst = @directory["Name_First"]
 	@namelast = @directory["Name_Last"]
 	@namefull = @namefirst +" "+ @namelast
