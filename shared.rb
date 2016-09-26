2b8a75210967abaf51ef3c8e5e950cd965ca6750
Kevin Kurpe <kevin@thinkgrasp.com>
2016-09-26 18:42:22 -0400

    Set the default environment to produciton. Moved the set_response and clear_response methods into the shared.rb file.

---
+++ shared.rb
@@ -0,0 +1,23 @@
+def set_response
+	@status = @statusCode
+	@body = @statusMessage
+end
+
+def clear_response
+	@theResponse = ""
+	@responseKind = ""
+	@responseCode = ""
+	@responseError = ""
+end
+
+def log_error_to_console
+	puts "\n\n\n\n\n"
+	puts "\n----------------------------------------"
+	puts "\n[DATABASE] #{@database}"
+	puts "\n[DIRECTORY] #{@directory_id}"
+	puts "\n[PAYMENTMETHOD] #{@payment_method_id}"
+	puts "\n#{@statusMessage}"
+	puts "\n[TIMESTAMP] #{Time.now.utc.iso8601}"
+	puts "\n----------------------------------------"
+	puts "\n\n\n\n\n"
+end
