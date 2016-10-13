7194e4cb100273bb95fbe99e3a9e1091550a37e4
Kevin Kurpe <kevin@thinkgrasp.com>
2016-10-13 15:03:32 -0400

    Moved the check_customer_token method to the shared.rb file. Enhaced the log_error_to_console method to output data specifically for either BC and PTD or DL databases.

--- shared.rb
+++ shared.rb
@@ -1,3 +1,11 @@
+def check_customer_token
+	if @customer_token != nil
+		@has_customer_token = true
+	else
+		@has_customer_token = false
+	end
+end
+
 def set_response
 	@status = @statusCode
 	@body = @statusMessage
@@ -14,8 +22,13 @@ def log_error_to_console
 	puts "\n\n\n\n\n"
 	puts "\n----------------------------------------"
 	puts "\n[DATABASE] #{@database}"
-	puts "\n[DIRECTORY] #{@directory_id}"
-	puts "\n[PAYMENTMETHOD] #{@payment_method_id}"
+	if @database == "BC" || @database == "PTD"
+		puts "\n[DIRECTORY] #{@directory_id}"
+		puts "\n[PAYMENTMETHOD] #{@payment_method_id}"
+	elsif @database == "DL"
+		puts "\n[LEAD] #{@lead_id}"
+		puts "\n[GUEST] #{@guest_id}"
+	end
 	puts "\n#{@statusMessage}"
 	puts "\n[CODE] #{@responseCode}"
 	puts "\n[REASON] #{@responseError}"
