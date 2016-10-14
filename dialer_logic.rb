e7f227874fed77f33f64a6686439e202728aecc0
Kevin Kurpe <kevin@thinkgrasp.com>
2016-10-13 20:40:16 -0400

    Almost completed the dialer logic file.

--- dialer_logic.rb
+++ dialer_logic.rb
@@ -1,29 +1,21 @@
-def process_create_dialier_payment_request
+def process_create_dialer_payment_method_request
 	parse_create_dialer_payment_post
 
-	if @requestType == "Charge"
+	if @request_type == "Charge"
 		create_dialer_tokens
 		process_dialer_payment
-	elsif @requestType == "Schedule"
+	elsif @request_type == "Schedule"
 		create_dialer_tokens
+		save_scheduled_dailer_payment
 	end
 
-	# Save the results into FileMaker.
-	save_dailerpayment
 end
 
-def create_dialer_tokens
-	if @recordtype = "DialerLead"
-
-	elsif @recordtype = "DialerGuest"
-		
-	end
-end
-
-def parse_create_dialer_payment_post
+def parse_create_dialer_payment_method_post
 	@lead_id = params[:lead_id]
 	@guest_id = params[:guest_id]
-	@requestType = params[:requestType]
+	@payment_method_id = params[:payment_method_id]
+	@request_type = params[:request_type] #Charge or Schedule
 
 	# Grab the values from the POST object.
 	@date = @params[:Date]
@@ -38,4 +30,22 @@ def parse_create_dialer_payment_post
 	@city = params[:Address_City]
 	@state = params[:Address_State]
 	@zip = params[:Address_Zip]
+end
+
+def create_dialer_tokens
+	if @recordtype = "DialerLead"
+		create_dialer_lead_customer_token
+		create_dialer_payment_token
+	elsif @recordtype = "DialerGuest"
+		# create_dialer_guest_customer_token
+		# create_dialer_payment_token
+	end
+end
+
+def process_dialer_payment
+	@step1 = ids_or_card
+	@step2 = process_payment
+	@step3 = report
+	@step4 = save_processed_dailer_payment
+	@step5 = clear
 end
