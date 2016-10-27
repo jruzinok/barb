0346630160a9c5375c0b466c2af56d569440c0fc
Kevin Kurpe <kevin@thinkgrasp.com>
2016-10-27 16:13:11 -0400

    Added the transaction attempt functionality. This functionality is design specifically to facilitate ambassadors to get customers caught up on their payments.

---
+++ transaction_attempt.rb
@@ -0,0 +1,99 @@
+require 'bigdecimal'
+
+def process_transaction_attempt
+
+	# This outputs the details of this Transacion Attempt to the console.
+	puts "\n\n\n\n\n"
+	puts "\n----------------------------------------"
+	puts "\n[TRANSACTION ATTEMPT]"
+	puts "\n[DATABASE] #{@database}"
+	puts "\n[DIRECTORY] #{@directory_id}"
+	puts "\n[PAYMENTMETHOD] #{@payment_method_id}"
+	puts "\n[DATE] #{@date}"
+	puts "\n[AMOUNT] #{@amount}"
+	puts "\n[TIMESTAMP] #{Time.now.utc.iso8601}"
+	puts "\n----------------------------------------"
+
+	find_directory
+	find_payment_method
+
+	process_onetime_payment
+end
+
+def process_onetime_payment
+	@step1 = set_gl_codes
+
+	if @directory_found == true && @payment_method_found == true	
+		@step2 = card_or_token
+		@step3 = process_payment
+		@step4 = log_result_to_console
+		@step5 = save_transaction_attempt
+	end
+
+	@step6 = set_response
+end
+
+def save_transaction_attempt
+	@transaction_attempt = PTDTransactionAttempt.new
+
+	# SAVE the response values for all transactions.
+	@transaction_attempt[:zzPP_Transaction] = @transactionID
+	@transaction_attempt[:zzPP_Response] = @theResponse
+	@transaction_attempt[:zzPP_Response_AVS_Code] = @avsCode
+	@transaction_attempt[:zzPP_Response_CVV_Code] = @cvvCode
+	@transaction_attempt[:zzPP_Response_Code] = @responseCode
+
+	# SET the foreign key fields.
+	@transaction_attempt[:_kF_Directory] = @directory_id
+	@transaction_attempt[:_kF_Statement] = @statement_id
+	@transaction_attempt[:_kF_PaymentMethod] = @payment_method_id
+
+	# RECORD the Transaction details.
+	@transaction_attempt[:Amount] = @amount
+	@transaction_attempt[:Date] = @date
+
+	# Record the transaction results for each processed payment.
+	if @resultCode == "OK"
+
+		if @responseKind == "Approved" || @transactionResponseCode == "1"
+			@transaction_attempt[:zzF_Status] = "Approved"
+			@transaction_attempt[:zzPP_Authorization_Code] = @authorizationCode
+			@transaction_attempt[:zzPP_Response_Message] = @responseMessage
+
+		elsif @responseKind == "Declined" || @transactionResponseCode == "2"
+			@transaction_attempt[:zzF_Status] = "Declined"
+			@transaction_attempt[:zzPP_Response_Error] = @responseError
+
+		elsif @responseKind == "Error" || @transactionResponseCode == "3"
+			@transaction_attempt[:zzF_Status] = "Error"
+			@transaction_attempt[:zzPP_Response_Error] = @responseError
+
+		elsif @responseKind == "HeldforReview" || @transactionResponseCode == "4"
+			@transaction_attempt[:zzF_Status] = "HeldForReview"
+			@transaction_attempt[:zzPP_Response_Error] = @responseError
+		end
+
+	# These payments were NOT processes.
+	else
+		if @responseKind == "TransactionError"
+			@transaction_attempt[:zzF_Status] = "Error"
+			@transaction_attempt[:zzPP_Transaction] = @transactionID
+
+			@transaction_attempt[:zzPP_Response] = @theResponse
+			@transaction_attempt[:zzPP_Response_Code] = @responseCode
+			@transaction_attempt[:zzPP_Response_Error] = @responseError
+
+		elsif @responseKind == "TokenError"
+			@transaction_attempt[:zzF_Status] = "TokenError"
+			@transaction_attempt[:zzPP_Response] = @theResponse
+			@transaction_attempt[:zzPP_Response_Code] = @responseCode
+			@transaction_attempt[:zzPP_Response_Error] = @responseError
+
+		elsif @responseKind == "TransactionFailure"
+			@transaction_attempt[:zzF_Status] = "Failure"
+			@transaction_attempt[:zzPP_Response_Error] = @responseError
+		end
+	end
+
+	@transaction_attempt.save
+end
